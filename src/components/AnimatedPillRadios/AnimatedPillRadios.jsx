1da464e015d1fd13ae136d984c870667a3961429
Artem Lenkevich <deal8906@gmail.com>
2021-05-26 20:37:22 +0300

    AnimatedPillRadios added color rotation

--- src/components/AnimatedPillRadios/AnimatedPillRadios.jsx
+++ src/components/AnimatedPillRadios/AnimatedPillRadios.jsx
@@ -1,43 +1,59 @@
-import { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styles from './AnimatedPillRadios.module.css'
 
 let properties = [
-    'hamburgers',
-    'hotdogs',
-    'salads'
-]
+    {
+      name: 'Hamburgers',
+      value: 'ham',
+      background: '#373e96'
+    },
+    {
+      name: 'Hotdogs',
+      value: 'hot',
+      background: '#e75252'
+    },
+    {
+      name: 'Salads',
+      value: 'sal',
+      background: '#2e8e65'
+    },
+  ];
 
 export const AnimatedPillRadios = () => {
     const [highlightProperties, setHighlightProperties] = useState(null);
-    const [activeToggle, setActiveToggle] = useState(properties[0]);
+    const [activeToggle, setActiveToggle] = useState(properties[0].name);
 
-    const toggles = properties.map(p => <Toggle setHighlightProperties={setHighlightProperties} 
-                                                name={p}
-                                                isActive={p === activeToggle}
+    const toggles = properties.map(p => <Toggle key={p.name}
+                                                name={p.name}
+                                                background={p.background}
+                                                isActive={p.name === activeToggle}
+                                                setHighlightProperties={setHighlightProperties} 
                                                 setActiveToggle={setActiveToggle}/>);
 
     return (
         <div className={styles.switcher}>
             <div className={styles.togglesContainer}>
                 {toggles}
-                <Highlight highlightProperties={highlightProperties}/>
+                {highlightProperties && <Highlight highlightProperties={highlightProperties}/>}
             </div>
         </div>
     )
 }
 
-const Toggle = ({name, setHighlightProperties, isActive, setActiveToggle}) => {
-    
-    const onToggleClick = (e) => {
-        const {offsetLeft: left, offsetTop: top, offsetWidth: width, offsetHeight: height} = e.currentTarget;
-        setHighlightProperties({left, top, width, height})
-        setActiveToggle(name)
-    }
+const Toggle = ({name, background, isActive, setHighlightProperties, setActiveToggle}) => {
+    const label = useMemo(() => React.createRef(), [])
+
+    useEffect(() => {
+        if (isActive) {
+            const {offsetLeft: left, offsetTop: top, offsetWidth: width, offsetHeight: height} = label.current;
+            setHighlightProperties({left, top, width, height, backgroundColor: background})
+        }
+    }, [isActive, label, background, setHighlightProperties])
 
     const labelClassName = isActive ? styles.radioLabel + ' ' + styles.radioLabelActive : styles.radioLabel ;
 
     return (
-        <label className={labelClassName} onClick={onToggleClick}>
+        <label ref={label} className={labelClassName} onClick={() => setActiveToggle(name)}>
             <input className={styles.radioInput} type='radio' value={name}/>
             {name}
         </label>
@@ -45,14 +61,5 @@ const Toggle = ({name, setHighlightProperties, isActive, setActiveToggle}) => {
 }
 
 const Highlight = ({highlightProperties}) => {
-
-    if (!highlightProperties) {
-        return <div className={styles.highlight}></div>
-    } else {
-        const {left, top, width, height} = highlightProperties;
-        const stylesObj = {left, top, width, height}
-
-        return <div className={styles.highlight} style={stylesObj}></div>
-    }
-
+        return <div className={styles.highlight} style={{...highlightProperties}}></div>
 }
